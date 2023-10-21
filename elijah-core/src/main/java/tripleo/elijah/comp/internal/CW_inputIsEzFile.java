b6aaa635e8257cf08174e8a041e854191316b165
Tripleo Nova Szi <szu@tripleo.milkyway>
2023-10-20 22:49:36 -0400

    nit: [CW_inputIsEzFile] Make not Sensable
    - see CB_FindCIs line 102

--- elijah-core/src/main/java/tripleo/elijah/comp/internal/CW_inputIsEzFile.java
+++ elijah-core/src/main/java/tripleo/elijah/comp/internal/CW_inputIsEzFile.java
@@ -1,26 +1,25 @@
 package tripleo.elijah.comp.internal;
 
-import org.jetbrains.annotations.*;
-import tripleo.elijah.comp.*;
-import tripleo.elijah.comp.i.*;
-import tripleo.elijah.sense.*;
-import tripleo.elijah.util.*;
+import org.jetbrains.annotations.NotNull;
+import tripleo.elijah.comp.CompilerInput;
+import tripleo.elijah.comp.i.CompilationClosure;
+import tripleo.elijah.comp.i.ILazyCompilerInstructions;
+import tripleo.elijah.util.Maybe;
 
-import java.util.function.*;
+import java.util.function.Consumer;
 
-public class CW_inputIsEzFile implements Sensable {
-	public void apply(final @NotNull CompilerInput input,
-					  final @NotNull CompilationClosure cc,
-					  final @NotNull Consumer<CompilerInput> x) {
-		final ILazyCompilerInstructions ilci = ILazyCompilerInstructions_.of(input, cc);
-
-		final Maybe<ILazyCompilerInstructions> m4 = Maybe.of(ilci);
+public class CW_inputIsEzFile /*implements Sensable*/ {
+	public static void apply(final @NotNull CompilerInput input,
+							 final @NotNull CompilationClosure cc,
+							 final @NotNull Consumer<CompilerInput> x) {
+		final ILazyCompilerInstructions        ilci = ILazyCompilerInstructions_.of(input, cc);
+		final Maybe<ILazyCompilerInstructions> m4   = Maybe.of(ilci);
 		input.accept_ci(m4);
 		x.accept(input);
 	}
 
-	@Override
-	public SenseIndex index() {
-		return SenseIndex.senseIndex_CW_inputIsEzFile;
-	}
+	//@Override
+	//public SenseIndex getSenseIndex() {
+	//	return SenseIndex.senseIndex_CW_inputIsEzFile;
+	//}
 }
