7a8f72bbce245b84abacaa08bcc8236526d4cf7b
tripleo1 <50311859+tripleo1@users.noreply.github.com>
2023-12-09 22:21:23 -0500

    7: Faddle

--- elijah-core/src/main/java/tripleo/elijah/stages/gen_fn/DependencyTracker.java
+++ elijah-core/src/main/java/tripleo/elijah/stages/gen_fn/DependencyTracker.java
@@ -9,13 +9,17 @@
 
 package tripleo.elijah.stages.gen_fn;
 
+import java.util.List;
+
+import tripleo.elijah.stages.deduce.FunctionInvocation;
+
 /**
  * Created 6/22/21 3:40 AM
  */
 public interface DependencyTracker {
 	// for use in DeduceTypes2
-//	List<GenType> dependentTypes();
-//	List<FunctionInvocation> dependentFunctions();
+	List<GenType> dependentTypes();
+	List<FunctionInvocation> dependentFunctions();
 }
 
 //
--- elijah-core/src/main/java/tripleo/elijah/stages/write_stage/pipeline_impl/LSPrintStream.java
+++ elijah-core/src/main/java/tripleo/elijah/stages/write_stage/pipeline_impl/LSPrintStream.java
@@ -15,72 +15,7 @@ import java.util.stream.Collectors;
 import static tripleo.elijah.util.Helpers.List_of;
 
 public class LSPrintStream implements XPrintStream {
-	public static final class LSResult {
-		private final List<String> buffer;
-		private final List<String> fs;
-
-		public LSResult(List<String> buffer, List<String> fs) {
-			this.buffer = buffer;
-			this.fs     = fs;
-		}
-
-		public List<EIT_Input> fs2(final Compilation c) {
-				return fs.stream().map(s -> new MyEIT_Input(c, s)).collect(Collectors.toList());
-			}
-
-			public List<EG_Statement> getStatement() {
-				return buffer.stream().map(str -> EG_Statement.of(str, EX_Explanation.withMessage("WriteBuffers")))
-						.collect(Collectors.toList());
-			}
-
-		public List<String> buffer() {
-			return buffer;
-		}
-
-		public List<String> fs() {
-			return fs;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (obj == this) return true;
-			if (obj == null || obj.getClass() != this.getClass()) return false;
-			var that = (LSResult) obj;
-			return Objects.equals(this.buffer, that.buffer) &&
-					Objects.equals(this.fs, that.fs);
-		}
-
-		@Override
-		public int hashCode() {
-			return Objects.hash(buffer, fs);
-		}
-
-		@Override
-		public String toString() {
-			return "LSResult[" +
-					"buffer=" + buffer + ", " +
-					"fs=" + fs + ']';
-		}
-
-		}
-
-	public static class MyEIT_Input implements EIT_Input {
-		private final Compilation c;
-		private final String       s;
-
-		public MyEIT_Input(final Compilation aC, final String aS) {
-			c = aC;
-			s = aS;
-		}
-
-		@Override
-		public EIT_InputType getType() {
-			return EIT_InputType.ELIJAH_SOURCE;
-		}
-	}
-
 	private final StringBuilder sb = new StringBuilder();
-
 	private final List<String> ff = new ArrayList<>();
 
 	public void addFile(final String aS) {
@@ -99,5 +34,69 @@ public class LSPrintStream implements XPrintStream {
 	public void println(final String aS) {
 		sb.append(aS);
 		sb.append('\n');
+	}
+
+	public static final class LSResult {
+		private final List<String> buffer;
+		private final List<String> fs;
+
+		public LSResult(List<String> buffer, List<String> fs) {
+			this.buffer = buffer;
+			this.fs     = fs;
+		}
+
+		public List<EIT_Input> fs2(final Compilation c) {
+			return fs.stream().map(s -> new MyEIT_Input(c, s)).collect(Collectors.toList());
+		}
+
+		public List<EG_Statement> getStatement() {
+			return buffer.stream().map(str -> EG_Statement.of(str, EX_Explanation.withMessage("WriteBuffers")))
+					.collect(Collectors.toList());
+		}
+
+		public List<String> buffer() {
+			return buffer;
+		}
+
+		public List<String> fs() {
+			return fs;
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(buffer, fs);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (obj == this) return true;
+			if (obj == null || obj.getClass() != this.getClass()) return false;
+			var that = (LSResult) obj;
+			return Objects.equals(this.buffer, that.buffer) &&
+					Objects.equals(this.fs, that.fs);
+		}
+
+		@Override
+		public String toString() {
+			return "LSResult[" +
+					"buffer=" + buffer + ", " +
+					"fs=" + fs + ']';
+		}
+
+	}
+
+	public static class MyEIT_Input implements EIT_Input {
+		private final Compilation c;
+		private final String      s;
+
+		public MyEIT_Input(final Compilation aC, final String aS) {
+			c = aC;
+			s = aS;
+		}
+
+		@Override
+		public EIT_InputType getType() {
+			return EIT_InputType.ELIJAH_SOURCE;
+		}
 	}
 }
