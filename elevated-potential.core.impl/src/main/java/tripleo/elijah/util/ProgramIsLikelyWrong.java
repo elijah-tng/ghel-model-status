dfd87fd71bc0768115654a66118c8e276d791a54
Tripleo <noble@tripleo.nonexistent>
2024-08-08 17:14:36 -0400

    2024-08-11 011: ProgramIsLikelyWrong: Add message constructor

--- elevated-potential.core.impl/src/main/java/tripleo/elijah/util/ProgramIsLikelyWrong.java
+++ elevated-potential.core.impl/src/main/java/tripleo/elijah/util/ProgramIsLikelyWrong.java
@@ -1,4 +1,11 @@
 package tripleo.elijah.util;
 
 public class ProgramIsLikelyWrong extends RuntimeException {
+	public ProgramIsLikelyWrong(final String aS) {
+		super(aS);
+	}
+
+	public ProgramIsLikelyWrong() {
+		super();
+	}
 }
