fcc69f8991d26835dbc630ea068cd45ed744f160
Tripleo <noble@tripleo.nonexistent>
2024-08-11 18:29:31 -0400

    2024-08-11 016b: hotfix: Missed a spot

--- elevated-potential.core.impl/src/main/java/tripleo/elijah/stages/write_stage/pipeline_impl/WPIS_WriteInputs.java
+++ elevated-potential.core.impl/src/main/java/tripleo/elijah/stages/write_stage/pipeline_impl/WPIS_WriteInputs.java
@@ -1,24 +1,16 @@
 package tripleo.elijah.stages.write_stage.pipeline_impl;
 
 import org.jetbrains.annotations.NotNull;
-import tripleo.elijah.comp.*;
-import tripleo.elijah.comp.graph.i.*;
+import tripleo.elijah.comp.IO_;
+import tripleo.elijah.comp.graph.i.CK_Monitor;
 import tripleo.elijah.nextgen.inputtree.EIT_Input_HashSourceFile_Triple;
-import tripleo.elijah.nextgen.outputstatement.EG_Naming;
-import tripleo.elijah.nextgen.outputstatement.EG_SequenceStatement;
-import tripleo.elijah.nextgen.outputstatement.EG_Statement;
+import tripleo.elijah.nextgen.outputstatement.*;
 import tripleo.elijah.nextgen.outputtree.*;
-import tripleo.elijah.util.Mode;
-import tripleo.elijah.util.Helpers;
-import tripleo.elijah.util.Ok;
-import tripleo.elijah.util.Operation;
+import tripleo.elijah.util.*;
 import tripleo.util.buffer.DefaultBuffer;
 import tripleo.util.buffer.TextBuffer;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static tripleo.elijah.util.Helpers.List_of;
 
@@ -69,7 +61,7 @@ public class WPIS_WriteInputs implements WP_Individual_Step {
 			String string = "inputs.txt";// pp.toFile().toString(); //fn1.getPath().toFile().toString();
 
 			final EOT_OutputFileImpl off = new EOT_OutputFileImpl(List_of(),
-																  new EOT_OutputFileImpl.DefaultFileNameProvider(string),
+																  new _U_OF.DefaultFileNameProvider(string),
 																  EOT_OutputType.INPUTS,
 																  seq);
 
