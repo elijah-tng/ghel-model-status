af84cd61bdf058fb0d3a17e48d27bdba9591f478
Tripleo <molasses@tripleo.milkyway>
2024-09-10 18:26:28 -0400

    fix: Missing case branch

--- xxx-elijah-core-api/elijah-core-api/src/main/java/tripleo/elijah/comp/i/ProgressSinkComponent.java
+++ xxx-elijah-core-api/elijah-core-api/src/main/java/tripleo/elijah/comp/i/ProgressSinkComponent.java
@@ -1,6 +1,6 @@
 package tripleo.elijah.comp.i;
 
-import org.jetbrains.annotations.*;
+import org.jetbrains.annotations.NotNull;
 
 // FIXME this is ugly
 public enum ProgressSinkComponent {
@@ -33,12 +33,29 @@ public enum ProgressSinkComponent {
 
 		@Override
 		public String printErr(final IProgressSink.Codes aCode, final int aType, final Object[] aParams) {
-			return switch (aType) {
-			case 5784 -> "*** DefaultCompilationBus ->> Start polling";
-			case 5757 -> "*** DefaultCompilationBus ->> %s".formatted(aParams[0]);
-			case 5758 -> "*** DefaultCompilationBus ->> poll returns null";
+			String result;
+			switch (aType) {
+			case 5784 -> {
+				/*DCB_Startable.DefaultCompilationBus_POLL_PROCESS*/
+				result = "*** DefaultCompilationBus ->> Start polling";
+			}
+			case 5757 -> {
+				result = "*** DefaultCompilationBus ->> %s".formatted(aParams[0]);
+				break;
+			}
+			case 5758 -> {
+				/*DCB_Startable.DefaultCompilationBus_POLL_PROCESS_EMPTY_BEGIN*/
+				result = "*** DefaultCompilationBus ->> poll returns null";
+				break;
+			}
+			case 5789 -> {
+				/*DCB_Startable.DefaultCompilationBus_POLL_PROCESS_EMPTY_END*/
+				result = "*** DefaultCompilationBus ->> poll process empty end";
+				break;
+			}
 			default -> throw new IllegalStateException("Unexpected value: " + aType);
-			};
+			}
+			return result;
 		}
 	};
 
